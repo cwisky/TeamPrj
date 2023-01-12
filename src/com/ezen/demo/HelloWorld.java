373928bbf761132467b98beb57e1be898f0d3c8d
cwisky <cwiskykim@gmail.com>
2023-01-12 11:42:09 +0900

    신림에서 오전 11:42

--- src/com/ezen/demo/HelloWorld.java
+++ src/com/ezen/demo/HelloWorld.java
@@ -5,10 +5,8 @@ public class HelloWorld
 	public static void main(String[] args) 
 	{
 		System.out.println("git test3, 집에서");
-
 		System.out.println("git test4, 집에서");
-
-		System.out.println("git test4, 신림에서");
-
+		System.out.println("git test5, 신림에서");
+		System.out.println("git test6, 신림에서");
 	}
 }
